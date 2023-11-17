44ce9fe47437df55db96e342fb97c15bce46da0d
Frank Stallone <frank@designzen.co>
2023-11-17 09:15:43 -0500

    config: ⚙️ Adding PurgeCSS, Prettier, and sitemap utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import purgecss from "astro-purgecss";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [purgecss(), sitemap()]
+});
