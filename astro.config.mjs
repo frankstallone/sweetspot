3e18e11df291542c235da633bfe5ed47c995f9f8
Frank Stallone <frank@designzen.co>
2023-11-22 08:37:21 -0500

    feat: ✨ Added site for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import purgecss from "astro-purgecss";
+import purgecss from 'astro-purgecss';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss(), sitemap()]
+  site: 'https://sweetspot.nyc',
+  integrations: [purgecss(), sitemap()],
 });
