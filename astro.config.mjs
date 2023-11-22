30b9b83d91781de86fb26d6b07801089de998dc4
Frank Stallone <frank@designzen.co>
2023-11-22 09:00:01 -0500

    feat: ✨ Added partytown and GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import purgecss from 'astro-purgecss';
-
 import sitemap from '@astrojs/sitemap';
+
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sweetspot.nyc',
-  integrations: [purgecss(), sitemap()],
+  integrations: [
+    purgecss(),
+    sitemap(),
+    partytown({
+      // Example: Add dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
